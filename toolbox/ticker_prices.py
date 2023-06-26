a31c934c6fde21ac7ff5a031943e89f9563fae4a
connor <connor.sw.personal@gmail.com>
2023-06-26 09:07:19 +0000

    Made ticker prices more efficient

--- toolbox/ticker_prices.py
+++ toolbox/ticker_prices.py
@@ -35,6 +35,88 @@ def set_storage_path(database_path: str, make_dir=False):
     database.set_storage_path(database_path)
 
 
+def _get_trend_request_(ticker, start, end, cooldown_counter=0, interval="1h", cooldown=True):
+    if cooldown:
+        time.sleep(3)
+    try:
+        trend = yf.download(ticker, start=start, end=end, interval=interval)
+        trend.index = pd.to_datetime(trend.index, utc=True)
+        trend.index = trend.index.tz_convert('America/New_York')
+    except Exception as e:
+        print(e)
+        if "404" in str(e):
+            print('404 error, returning None')
+            return None
+        else:
+            print('Error, retrying in 5 seconds')
+            time.sleep(5 * cooldown_counter)
+            if cooldown_counter < 5:
+                return _get_trend_request_(start, end, cooldown_counter + 1, interval=interval)
+            else:
+                print('Cooldown counter reached, returning None')
+                return None
+    return trend
+
+
+def _get_trend_(ticker, start_date, end_date, cooldown=True):
+    start_end_dates = []
+    pre_existing_trend = None
+    start_date_none = start_date is None
+    if start_date_none:
+        # Set to 5 years ago
+        start_date = end_date - datetime.timedelta(days=365 * 5)
+    while start_date < end_date:
+        a = start_date+datetime.timedelta(days=365)
+        interval = "1h"
+        # If the time delta is greater than 2 years, use 1d interval
+        if (end_date - start_date).days > 365 * 2-1:
+            interval = "1d"
+        days = 365
+        if (end_date - start_date).days < days:
+            start_end_dates.append((start_date, end_date, interval))
+            break
+        else:
+            start_end_dates.append((start_date, start_date + datetime.timedelta(days=days), interval))
+            start_date = start_date + datetime.timedelta(days=days)
+
+    # Combine all dates with "1d" intervals
+    new_start_end_dates = []
+    earliest_start_date = None
+    latest_end_date = None
+    for i, value in enumerate(start_end_dates):
+        # Find earliest date with "1d" interval
+        if value[2] == "1d":
+            if earliest_start_date is None:
+                earliest_start_date = value[0]
+            else:
+                if value[0] < earliest_start_date:
+                    earliest_start_date = value[0]
+            # Find latest date with "1d" interval
+            if latest_end_date is None:
+                latest_end_date = value[1]
+            else:
+                if value[1] > latest_end_date:
+                    latest_end_date = value[1]
+    if start_date_none:
+        earliest_start_date = None
+    if latest_end_date is not None:
+        new_start_end_dates.append((earliest_start_date, latest_end_date, "1d"))
+    for i, value in enumerate(start_end_dates):
+        if value[2] != "1d":
+            new_start_end_dates.append(value)
+    start_end_dates = new_start_end_dates
+    for start, end, interval in start_end_dates:
+        if end > end_date:
+            end = end_date
+        if pre_existing_trend is None:
+            pre_existing_trend = _get_trend_request_(ticker, start, end, interval=interval, cooldown=cooldown)
+            if pre_existing_trend is None:
+                return None
+        else:
+            pre_existing_trend = pd.concat([pre_existing_trend, _get_trend_request_(ticker, start, end, interval=interval, cooldown=cooldown)])
+        time.sleep(1)
+    return pre_existing_trend
+
 def get_ticker_historical_trend(ticker: str, start_date: datetime.datetime = None, end_date: datetime.datetime = None, cooldown=True, database_only=False) -> pd.DataFrame:
     """
     Params
@@ -87,102 +169,12 @@ def get_ticker_historical_trend(ticker: str, start_date: datetime.datetime = Non
     #    end_date = end_date - datetime.timedelta(days=historical_buffer_days)
 
 
-    def get_trend_request(ticker, start, end, cooldown_counter=0, interval="1h"):
-        if cooldown:
-            time.sleep(3)
-        try:
-            trend = yf.download(ticker, start=start, end=end, interval=interval)
-            trend.index = pd.to_datetime(trend.index, utc=True)
-            trend.index = trend.index.tz_convert('America/New_York')
-        except Exception as e:
-            print(e)
-            if "404" in str(e):
-                print('404 error, returning None')
-                return None
-            else:
-                print('Error, retrying in 5 seconds')
-                time.sleep(5 * cooldown_counter)
-                if cooldown_counter < 5:
-                    return get_trend_request(start, end, cooldown_counter + 1, interval=interval)
-                else:
-                    print('Cooldown counter reached, returning None')
-                    return None
-        return trend
-
-    def get_trend(ticker, start_date, end_date):
-        start_end_dates = []
-        pre_existing_trend = None
-
-        start_date_none = start_date is None
-        if start_date_none:
-            # Set to 5 years ago
-            start_date = end_date - datetime.timedelta(days=365 * 5)
-        while start_date < end_date:
-            a = start_date+datetime.timedelta(days=365)
-
-            interval = "1h"
-
-            # If the time delta is greater than 2 years, use 1d interval
-            if (end_date - start_date).days > 365 * 2-1:
-                interval = "1d"
-
-            days = 365
-            if (end_date - start_date).days < days:
-                start_end_dates.append((start_date, end_date, interval))
-                break
-            else:
-                start_end_dates.append((start_date, start_date + datetime.timedelta(days=days), interval))
-                start_date = start_date + datetime.timedelta(days=days)
 
 
-        # Combine all dates with "1d" intervals
-        new_start_end_dates = []
-        earliest_start_date = None
-        latest_end_date = None
-        for i, value in enumerate(start_end_dates):
-            # Find earliest date with "1d" interval
-            if value[2] == "1d":
-                if earliest_start_date is None:
-                    earliest_start_date = value[0]
-                else:
-                    if value[0] < earliest_start_date:
-                        earliest_start_date = value[0]
-
-                # Find latest date with "1d" interval
-                if latest_end_date is None:
-                    latest_end_date = value[1]
-                else:
-                    if value[1] > latest_end_date:
-                        latest_end_date = value[1]
-
-        if start_date_none:
-            earliest_start_date = None
-
-        if latest_end_date is not None:
-            new_start_end_dates.append((earliest_start_date, latest_end_date, "1d"))
-
-        for i, value in enumerate(start_end_dates):
-            if value[2] != "1d":
-                new_start_end_dates.append(value)
-
-        start_end_dates = new_start_end_dates
-
-        for start, end, interval in start_end_dates:
-            if end > end_date:
-                end = end_date
-            if pre_existing_trend is None:
-                pre_existing_trend = get_trend_request(ticker, start, end, interval=interval)
-                if pre_existing_trend is None:
-                    return None
-            else:
-                pre_existing_trend = pd.concat([pre_existing_trend, get_trend_request(ticker, start, end, interval=interval)])
-            time.sleep(1)
-
-        return pre_existing_trend
 
     pre_existing_trend = database.get(ticker + '_trend')
     if pre_existing_trend is None:
-        pre_existing_trend = get_trend(ticker, start_date, end_date)
+        pre_existing_trend = _get_trend_(ticker, start_date, end_date, cooldown=cooldown)
     else:
         # Get the last date in the pre-existing trend
         last_date = pre_existing_trend.index[-1]
@@ -199,7 +191,7 @@ def get_ticker_historical_trend(ticker: str, start_date: datetime.datetime = Non
             end_date = end_date - datetime.timedelta(days=2)
 
         if last_date < end_date and not database_only:
-            trend = get_trend(ticker, last_date, end_date)
+            trend = _get_trend_(ticker, last_date, end_date, cooldown=cooldown)
             if trend is None:
                 return pre_existing_trend
             else:
