69e7648b5eb7f44cbbc0a980b87c4d45be473786
connor <connor.sw.personal@gmail.com>
2023-06-28 05:14:22 +0000

    Google Chrome Selenium Webscraping

--- toolbox/web_scraping_utility.py
+++ toolbox/web_scraping_utility.py
@@ -1,14 +1,30 @@
+import os, time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
 # Add headless option
 options = webdriver.ChromeOptions()
 options.add_argument('--headless=new')
 
+def test_headless(website, screenshot_path, screen_width=1920, screen_height=1080):
 
-driver = webdriver.Chrome(options=options)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
 
-driver.get('https://www.google.com')
+    # Initialize the Chrome webdriver and pass the options
+    driver = webdriver.Chrome(options=options)
 
-driver.quit()
+    driver.set_window_size(screen_width, screen_height)
 
+    driver.get(website)
+
+    # If path doesn't have a .png extension, add it
+    if not screenshot_path.endswith('.png'):
+        screenshot_path += '.png'
+
+    # Take screenshot
+    driver.save_screenshot(screenshot_path)
+
+    driver.quit()
+
+
+if __name__ == '__main__':
+    test_headless('https://www.google.com', 'test')
